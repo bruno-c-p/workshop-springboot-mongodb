eb2b457359eaa8716f17d8f6030e8692a45b59ad
bruno-c-p <bruno.cardozo.pereira@gmail.com>
2022-08-30 19:28:42 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/brunocp/workshopmongo/resources/UserResource.java
+++ src/main/java/com/brunocp/workshopmongo/resources/UserResource.java
@@ -1,28 +1,28 @@
 package com.brunocp.workshopmongo.resources;
 
 import com.brunocp.workshopmongo.domain.User;
+import com.brunocp.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
 
-        User user = new User("1", "Maria Brown", "maria@gmail.com");
-        User user2 = new User("2", "Alex Green", "maria@gmail.com");
+        List<User> list = service.findAll();
 
-        return ResponseEntity.ok().body(Arrays.asList(user, user2));
+        return ResponseEntity.ok().body(list);
     }
 }
