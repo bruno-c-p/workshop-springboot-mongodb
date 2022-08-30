6cd921bf3ace200b4df78b931b84c5f601b94915
bruno-c-p <bruno.cardozo.pereira@gmail.com>
2022-08-30 17:49:48 -0300

    Primeiro commit - Projeto criado

---
+++ src/test/java/com/brunocp/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunocp.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
